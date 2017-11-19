2fab1c55b4f35a9926839dabff874bf8a70fc35a
kaashmonee <skandakk@gmail.com>
2017-11-19 03:01:57 -0500

    updating somethings

---
+++ learncv/playwithpixels.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+
+def playWithPixels():
+    # getting the image
+    img = cv2.imread("./assets/basketball.png")
+
+    print(type(img))
+
+    # returns numpy array with BGR values at specified pixel location
+    px = img[100, 100]
+
+    # accessing only the blue pixel (not sure how this works)
+    # specifically, what is the 3rd parameter? oh shit nevermind, i get it now
+    # the third parameter is the parameter associated with B=0,G=1,R=2
+    blue = img[100, 100, 0]
+    print(blue)
+
+    ## Modifying pixels ##
+
+    # modifies the red value of the pixel at 10, 10 to 100
+    img.itemset((10, 10, 2), 100)
+
+    print("red value after setting:", img.item(10, 10, 2))
+
+    print(px)
+
+
+def main():
+    playWithPixels()
+
+
+if __name__ == "__main__":
+    main()
--- learncv/readimage.py
+++ learncv/readimage.py
@@ -1,17 +1,32 @@
 import numpy as np
 import cv2
+import random
 
-# load a color image as grayscale
-img = cv2.imread("./assets/basketball.png", 0)
-#print(img)
-# try:
-cv2.imshow("image", img)
-cv2.waitKey()
-print("reaching here")
-    
-# except Exception as e:
-#     print("reaching except statement")
-#     print(e)
 
-# print(cv2.__version__)
+def loadImage():
+    # load a color image as grayscale
+    img = cv2.imread("./assets/basketball.png", 0)
 
+    # showing the image
+    cv2.imshow("image", img)
+    # waiting indefinitely for a keystroke, and captures keystroke
+    k = cv2.waitKey(0)
+    # depending on what k is, it does certain things
+    # if k = 27, (esc key) it destroys everything
+    if k == 27:
+        cv2.destroyAllWindows()
+    # if k is the s key, it will save and close windows
+    elif k == ord("s"):
+        cv2.imwrite("./assets/img_grascale.png", img)
+        cv2.destroyAllWindows()
+    print("reaching here")
+
+
+def main():
+    functions = dict()
+    functions[1] = loadImage
+    functions[1]()
+
+
+if __name__ == "__main__":
+    main()

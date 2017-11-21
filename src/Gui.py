c67947d99da398823eeef61b0df6d421dba1ae90
kaashmonee <skandakk@gmail.com>
2017-11-21 15:54:43 -0500

    found and changed icon, created icon class attribute

--- src/Gui.py
+++ src/Gui.py
@@ -1,20 +1,31 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
+# QtCore is used for event handling things
 
 
 
 class SongDash(QtGui.QWidget):
+    MUSIC_ICON = "./src/assets/musicalnote.png"
     def __init__(self):
         super().__init__()
         # determines where on the screen it it will show up and how big the 
         # screen should be
         self.setGeometry(50, 50, 500, 500)
         self.setWindowTitle("SongBox")
-        # setting a window icon
-        self.setWindowIcon(QtGui.QIcon("./learncv/assets/images/lebron.jpg"))
+        # setting a window icon (definitely change later)
+        self.setWindowIcon(QtGui.QIcon(SongDash.MUSIC_ICON))
+        self.home()
     
     # shows the window
     def _show(self): self.show()
+
+    # this is for a particular page in the GUI (the home page)
+    def home(self):
+        btn = QtGui.QPushButton("Quit", self)
+        # if the button is clicked, finds instance of QCore application and 
+        # quits
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.show()
 
 
 # main function for testing purposes

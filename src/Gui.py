4ed0ca54a18dedffac6a8a33fc38bc76aba7f2d5
kaashmonee <skandakk@gmail.com>
2017-11-21 15:40:23 -0500

    created own song box gui using pyqt4

--- src/Gui.py
+++ src/Gui.py
@@ -2,11 +2,25 @@ import sys
 from PyQt4 import QtGui
 
 
-app = QtGui.QApplication(sys.argv)
 
-window = QtGui.QWidget()
-print("reachign here")
+class SongDash(QtGui.QWidget):
+    def __init__(self):
+        super().__init__()
+        # determines where on the screen it it will show up and how big the 
+        # screen should be
+        self.setGeometry(50, 50, 500, 500)
+        self.setWindowTitle("SongBox")
+    
+    # shows the window
+    def _show(self): self.show()
 
-window.show()
-print("something not happening here.")
-app.exec_()
+
+# main function for testing purposes
+def main(): 
+    app = QtGui.QApplication(sys.argv)
+    dash = SongDash()
+    dash._show()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()

5bfd69d8dbe9376243f2050fb5b1d7f7ed3eae6c
kaashmonee <skandakk@gmail.com>
2017-11-29 00:55:54 -0500

    added welcome.py

---
+++ src/welcome.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'welcome.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(750, 508)
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        Form.setFont(font)
+        self.verticalLayoutWidget = QtGui.QWidget(Form)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 751, 511))
+        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Nimbus Mono L"))
+        font.setPointSize(72)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setUnderline(False)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setMouseTracking(True)
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
+        self.pushButton_3 = QtGui.QPushButton(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Nimbus Mono L"))
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.verticalLayout.addWidget(self.pushButton_3)
+        self.pushButton_2 = QtGui.QPushButton(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Nimbus Mono L"))
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.verticalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily(_fromUtf8("Nimbus Mono L"))
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.label.setText(_translate("Form", "songbox", None))
+        self.pushButton_3.setText(_translate("Form", "Take Picture", None))
+        self.pushButton_2.setText(_translate("Form", "Help", None))
+        self.pushButton.setText(_translate("Form", "Contact Us", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
